5b9a232104a91a3331bb4eb94deb31b43c7084d9
WoodenNebula <87257970+WoodenNebula@users.noreply.github.com>
2023-10-26 19:58:17 +0545

    made a pre-compiled header file that can be used to reduce compile time (not implemented yet)

---
+++ OpenGL/src/stdpch.h
@@ -0,0 +1,17 @@
+#pragma once
+// This header includes all the commonly used STL header files which are to be configured later on to be pre-compiled
+
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <memory>
+#include <utility>
+#include "Assert.h"
+
+
+// Data Structures
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
